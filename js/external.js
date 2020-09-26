079fc0ab570ff2a4ac50260d23b00b05ae18b28c
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-09-25 20:38:30 -0500

    js in html exercises

---
+++ js/external.js
@@ -0,0 +1,64 @@
+"use strict"
+
+console.log("Hello from external JavaScript.");
+
+alert("Welcome to my website!");
+
+let userInput = prompt('What is your favorite color?');
+
+alert("Awesome, " + userInput + " is my favorite color too!");
+
+//Question 3 Part 1
+var bb = Number(prompt('How many days are you renting Brother Bear for?'));
+
+var lm = Number(prompt('How many days are you renting Little Mermaid for?'));
+
+var h = Number(prompt('How many days are you renting Hercules for?'));
+
+var ppd = prompt('How much does it cost to rent a video per day?');
+
+
+var cost
+
+cost= (lm + bb + h) * ppd;
+
+alert("The total cost rent the movies is $" + cost);
+
+//Question 3 Part 2
+var Gpay = Number(prompt('How much does Google pay per hour?'));
+var Apay = Number(prompt('How much does Amazon pay per hour?'));
+var Fpay = Number(prompt('How much does Facebook pay per hour?'));
+var Ghr = Number(prompt('How many hours did you work?'));
+var Fhr = Number(prompt('How many hours did you work?'));
+var Ahr = Number(prompt('How many hours did you work?'));
+
+var pay;
+pay = (Gpay * Ghr) + (Fpay * Fhr) + (Apay * Ahr);
+
+alert("Your total pay is $" + pay);
+
+//Question 3 Part 3
+var classFull = Number(prompt('What is the max class size?'));
+var classSize = Number(prompt('How many students are in the class?'));
+var classOpen = Number(y<x);
+var noConflicts = true;
+var enrolled = true;
+if (classFull && noConflicts){
+    enrolled
+}
+
+
+//Question 3 Part 4
+var numOfItems = x;
+var customer = y;
+var offer = true;
+var status = true;
+Function
+if (numOfItems > 2 && offer === true) {
+    return true;
+}
+if (status === true) {
+    return true;
+} else {
+    return false;
+}
