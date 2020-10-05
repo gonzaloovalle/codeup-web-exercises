c35bb0970207e717536a89b953e9de2b427b400a
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-10-05 10:36:36 -0500

    Completed Do While Loop exercise

--- js/do_while.js
+++ js/do_while.js
@@ -3,14 +3,21 @@
 
     var allCones = Math.floor(Math.random() * 50) + 50;
 
-    var i = 0
-
     do {
+        console.log("There is a total of " + allCones + " cones.");
         var conesSold = Math.floor(Math.random() * 5) + 1;
-        console.log(conesSold + " cones sold");
-        i++
+        if(conesSold < allCones){
+            console.log(conesSold + " cones sold.");
+            allCones -= conesSold;
+        }else if(conesSold > allCones){
+            console.log("I cannot sell you " + conesSold + ", I only have " + allCones + " left.");
+            allCones -= conesSold;
+        }else if(conesSold === allCones){
+            console.log("Yay! I sold the last " + conesSold + "! I can go home now!");
+            break;
+        }
 
-    }while(i < allCones)
+    }while(allCones > 0);
 
 
 
