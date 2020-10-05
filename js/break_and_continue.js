f131ee61b42c70b302ac6967bec7eaf403f589b4
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-10-05 12:15:51 -0500

    Completed Break and Continue Loop exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,26 +1,31 @@
 (function(){
     "use strict"
 
-    var l = parseInt(prompt("Enter a number between 1 and 50"));
-
-    for (var i = 1; i <= 50; i++){
-        if (l <= 1 || l >= 50){
+    do{
+        var userNum = parseInt(prompt("Pick an odd number between 1 and 50"))
+        if(userNum < 1 || userNum > 50){
+            alert("Number is invalid. Please try again.");
+        } else if(userNum % 2 === 0){
+            alert(userNum + " is not odd, please try again.");
+        } else if(isNaN(userNum)){
+            alert(userNum + " is not a number, please try again.")
+        } else {
+            alert("Number is valid. Thank you!")
             break;
         }
+    }while(true)
 
+    for (var i = 1; i <= 50; i++){
+        if(i % 2 === 0){
+            continue;
+        }
+
+        if(i === userNum){
+            console.log("Yikes! Skipping number " + i);
+        } else {
+            console.log("Here is an odd number: " + i);
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 })();
