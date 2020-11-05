bc437b1804804546a091876393357e911efe0d57
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-11-05 14:25:51 -0600

    completed mapbox restaurants exercise

--- js/restaurant_map.js
+++ js/restaurant_map.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
     center: [-98.5347, 29.5567], // starting position [lng, lat]
-    zoom: 12 // starting zoom
+    zoom: 10 // starting zoom
 });
 
 
@@ -13,35 +13,65 @@ var markerOptions = {
 };
 
 
-geocode("3330 Culebra Rd, San Antonio, TX 78228", mapboxToken).then(function(result) {
-    console.log(result);
-    map.setZoom(10);
+// geocode("3330 Culebra Rd, San Antonio, TX 78228", mapboxToken).then(function(result) {
+//     console.log(result);
+//     map.setZoom(10);
+//
+//     var marker = new mapboxgl.Marker(markerOptions)
+//         .setLngLat(result)
+//         .addTo(map)
+//         .setPopup(new mapboxgl.Popup().setHTML("<p>La Sorrentina</p>"));
+//
+// });
+//
+// geocode("700 E Sonterra Blvd #1117, San Antonio, TX 78258", mapboxToken).then(function(result) {
+//     console.log(result);
+//     map.setZoom(10);
+//
+//     var marker = new mapboxgl.Marker(markerOptions)
+//         .setLngLat(result)
+//         .addTo(map)
+//         .setPopup(new mapboxgl.Popup().setHTML("<p>Smashin Crab</p>"));
+//
+// });
+//
+// geocode("4230 McCullough Ave #2, San Antonio, TX 78212", mapboxToken).then(function(result) {
+//     console.log(result);
+//     map.setZoom(10);
+//
+//     var marker = new mapboxgl.Marker(markerOptions)
+//         .setLngLat(result)
+//         .addTo(map)
+//         .setPopup(new mapboxgl.Popup().setHTML("<p>Pho Kim Long</p>"));
+//
+// });
 
-    var marker = new mapboxgl.Marker(markerOptions)
-        .setLngLat(result)
-        .addTo(map)
-        .setPopup(new mapboxgl.Popup().setHTML("<p>La Sorrentina</p>"));
 
-});
 
-geocode("700 E Sonterra Blvd #1117, San Antonio, TX 78258", mapboxToken).then(function(result) {
-    console.log(result);
-    map.setZoom(10);
+var restArray = [
+    {
+        address: "3330 Culebra Rd, San Antonio, TX 78228",
+        popupHTML: "<p>La Sorrentina\n</p>"
+    },
+    {
+        address: "700 E Sonterra Blvd #1117, San Antonio, TX 78258",
+        popupHTML: "<p>Smashin Crab\n</p>"
+    },
+    {
+        address: "4230 McCullough Ave #2, San Antonio, TX 78212",
+        popupHTML: "<p>Pho Kim Long\n</p>"
+    }
+]
 
-    var marker = new mapboxgl.Marker(markerOptions)
-        .setLngLat(result)
-        .addTo(map)
-        .setPopup(new mapboxgl.Popup().setHTML("<p>Smashin Crab</p>"));
+restArray.forEach(function (rest) {
+    console.log(rest);
+    geocode(rest.address, mapboxToken).then(function(coordinates) {
+        var popup = new mapboxgl.Popup()
+            .setHTML(rest.popupHTML);
 
-});
-
-geocode("4230 McCullough Ave #2, San Antonio, TX 78212", mapboxToken).then(function(result) {
-    console.log(result);
-    map.setZoom(10);
-
-    var marker = new mapboxgl.Marker(markerOptions)
-        .setLngLat(result)
-        .addTo(map)
-        .setPopup(new mapboxgl.Popup().setHTML("<p>Pho Kim Long</p>"));
-
+        var marker = new mapboxgl.Marker(markerOptions)
+            .setLngLat(coordinates)
+            .setPopup(popup)
+            .addTo(map);
+    });
 });
