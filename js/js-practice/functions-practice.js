8fd62c1e3c372ce265d72c5c9b126da742761a01
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-02-08 22:05:19 -0600

    functions practice

---
+++ js/js-practice/functions-practice.js
@@ -0,0 +1,18 @@
+function greet() {
+    return 'Haydo!';
+}
+
+let salutation = greet();
+
+console.log(salutation);
+
+function echo(input) {
+    return input;
+}
+
+let result = echo('Greta');
+
+let result2 = echo('CO2');
+
+console.log(result);
+console.log(result2);
