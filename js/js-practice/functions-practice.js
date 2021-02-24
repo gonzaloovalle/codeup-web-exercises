ec44b2e2529ae005cee8d8eec5701e07a09c790a
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-02-23 21:02:44 -0600

    functions practice

--- js/js-practice/functions-practice.js
+++ js/js-practice/functions-practice.js
@@ -16,3 +16,28 @@ let result2 = echo('CO2');
 
 console.log(result);
 console.log(result2);
+
+function reverse_number(n) {
+    n = n + ""
+    return n.split("").reverse().join("");
+}
+
+console.log(reverse_number(2345));
+
+function alphabetical(str) {
+    return str.split("").sort().join("");
+}
+
+console.log(alphabetical("hello"));
+
+function uppercase(str) {
+    let arr = str.split(" ");
+    let newarr = [];
+
+    for(var x = 0; x < arr.length; x++) {
+        newarr.push(arr[x].charAt(0).toUpperCase()+arr[x].slice(1))
+    }
+    return newarr.join(" ");
+}
+
+console.log(uppercase("the quick brown fox"));
