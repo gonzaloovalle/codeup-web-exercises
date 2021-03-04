54a593b1566a988464d1e1eb5421a9fc83a56ce8
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-03-03 20:50:26 -0600

    functions practice

--- js/js-practice/functions-practice.js
+++ js/js-practice/functions-practice.js
@@ -41,3 +41,23 @@ function uppercase(str) {
 }
 
 console.log(uppercase("the quick brown fox"));
+
+function factors(n)
+{
+    let num_factors = [], i;
+
+    for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
+        if (n % i === 0)
+        {
+            num_factors.push(i);
+            if (n / i !== i)
+                num_factors.push(n / i);
+        }
+    num_factors.sort(function(x, y)
+    {
+        return x - y;});
+    return num_factors;
+}
+console.log(factors(15));
+console.log(factors(16));
+console.log(factors(17));
